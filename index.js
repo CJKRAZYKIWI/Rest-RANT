51245b2a8e531601455bbf8bbf288b6e7d0f8e02
Cj Galacgac <galacc1@unlv.nevada.edu>
2022-01-12 20:39:20 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Require needed modules.
+const express = require('express')
+
+// Initialize the app object.
+const app = express()
+
+// Create a homepage route.
+app.get('/', function (req, res) {
+    // This gets sent to the client 
+    // (your web browser most likely!)
+    res.send('Hello world')
+})
+
+app.listen(3001)
