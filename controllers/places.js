5f8ed2afdb387bcf2f26c454557eaba9aba96270
Cj Galacgac <galacc1@unlv.nevada.edu>
2022-01-21 14:29:12 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -20,4 +20,18 @@ router.get('/',(req,res)=> {
     res.render('places/index', {places})
 })
 
+//show route
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if(!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/show', { place: places[id], id })
+    }
+    
+})
 module.exports = router
