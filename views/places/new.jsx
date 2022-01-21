406df0d47368865c88e81eaf6bdbbd53f427e891
Cj Galacgac <galacc1@unlv.nevada.edu>
2022-01-21 12:59:05 -0800

    Set up design form classes for CSS design down the road

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,11 +1,29 @@
 const React = require('react')
 const Def = require('../default')
 
+//CSSstyle this when blueprint is done
 function new_form () {
     return (
         <Def>
             <main>
                 <h1> Add a New Place </h1>
+                <form method = "POST" action= "/places">
+                    <div className = "form-group">
+                        <label htmlFor="name"> Place Name </label>
+                        <input id="name" name ="name" required />
+                        <label htmlFor="pic"> Place Pic </label>
+                         <input type="url" id="pic" name ="pic" />
+                        
+                        <label htmlFor="Location"> City,State </label>
+                       <input id="Location" name ="Location" required />
+                       
+                        <label htmlFor="cuisines"> Type of cuisine </label>
+                         <input id="cuisines" name ="cuisines" required  />
+                        
+                    </div>
+                    <input className = "btn btn-primaty" type = "submit" value= "Add Place" />
+
+                </form>
             </main>
         </Def>
     )
