904d672808443930f24b4221ff2e6dc4c1ab24af
Cj Galacgac <galacc1@unlv.nevada.edu>
2022-01-21 21:37:35 -0800

    cant delete post and redirrect

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,23 @@
 const React = require('react')
+const methodOverride = require ('method-override')
 const Def = require('../default')
 
-function show () {
+//this is designing the show page
+
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{data.place.name }</h1>
           </main>
+          <div>
+            <a href={'/places/${data.id}/edit'} className="btn btn-warning"> 
+  Edit
+</a>
+<form method="POST" action= {'/places/${data.id}?_method=DELETE'}>
+  <button type= "submit" className="btn btn-danger">Delete</button>
+</form>
+ </div>
         </Def>
     )
 }
