5a967ecaf32c180b8f5bbe714edd4b660d3627e1
Cj Galacgac <galacc1@unlv.nevada.edu>
2022-01-24 16:50:10 -0800

    added pictures for the home page in the files

--- views/home.jsx
+++ views/home.jsx
@@ -3,15 +3,21 @@ const Def = require( './default')
 
 function home () {
     return (
-        <Def>
-            <main>
-                <h1>HOME SKILLET</h1>
-                <a href = "/places">
-                    <button className="btn-primary">Places Page </button>
-                </a>
-            </main>
-        </Def>
-    )
-}
+        <html> 
+            <head>
+                <meta name ="viewport" content= "width=device-width, initial-scale = 1.0"></meta>
+                <title> Home Page</title>
+                <link rel = "stylesheet" href= "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
+                <link rel = "stylesheet" href = "/css/home.css" />
+            </head>
+            <body>
+              <div className ="outer-wrapper"></div>
+                <div className ="wrapper"></div>
+                    <div className = "one"></div>
+                    <div className = "two"></div>
+                    <div className = "three"></div>
+            </body>
+        </html>
 
+    )}
 module.exports = home
