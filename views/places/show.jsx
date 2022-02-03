c4f8afa0078da94c0c7a1778793c692a65ad65f4
Cj Galacgac <galacc1@unlv.nevada.edu>
2022-02-02 18:56:31 -0800

    added stars and ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,8 +9,26 @@ function show (data) {
     <h3 className= "inactive">
       No comments yet!
     </h3>
+    )
+    let rating = (
+        <h3 className="inactive">
+          Not yet rated!
+        </h3>
   )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐️'
+    }
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
     comments = data.place.comments.map (c => {
       return (
         <div className= "border">
@@ -39,6 +57,7 @@ function show (data) {
             <h2>
               Description
             </h2>
+            <h2> Rating</h2> {rating}
             <h2> Comments </h2>{comments}
             <h3>
               {data.place.showEstablished()}
