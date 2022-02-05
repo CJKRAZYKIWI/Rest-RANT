de898961cebc5750c26ae893a3c0a206d9449432
Cj Galacgac <galacc1@unlv.nevada.edu>
2022-02-05 03:07:27 -0800

    home page has been styled but i want to add onscroll events still

--- views/default.jsx
+++ views/default.jsx
@@ -1,5 +1,18 @@
 const React = require('react')
-
+import {
+    MDBNavbar,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBNavbarToggler,
+    MDBContainer,
+    MDBIcon,
+    MDBCardText
+  } from 'mdb-react-ui-kit';
+  import Image from 'react-bootstrap/Image'
+  import { MDBRipple } from 'mdb-react-ui-kit';
+  import { MDBBtn } from 'mdb-react-ui-kit'; 
+  
 function Def (html) {
     return (
         <html>
@@ -27,19 +40,40 @@ function Def (html) {
     crossorigin=""></script>
             </head>
             <body>
-                <nav> 
-                    <ul>
-                        <li>
-                            <a href= "/">Home</a>
-                        </li>
-                        <li>
-                            <a href= "/places">Places</a>
-                        </li>
-                        <li>
-                            <a href= "/places/new">Add Place</a>
-                        </li>
-                    </ul>
-                </nav>
+            <header>
+      <MDBNavbar expand='lg' light bgColor='white' sticky>
+      <MDBContainer fluid>
+        <span className='navbar-text'> An app better than Yelp with puns that help </span>
+      </MDBContainer>
+        <MDBContainer fluid>
+          <MDBNavbarToggler
+            aria-controls='navbarExample01'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <MDBIcon fas icon='bars' />
+          </MDBNavbarToggler>
+          <div className='collapse navbar-collapse ' id='navbarExample01'>
+            <MDBNavbarNav right className='mb-2 mb-lg-0   justify-content-center'>
+              <MDBNavbarItem active>
+                <MDBNavbarLink aria-current='page' href='/'>
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>About</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </div>
+        </MDBContainer>
+      </MDBNavbar>
+      </header>
                 {html.children}
             </body>
         </html>
--- views/home.jsx
+++ views/home.jsx
@@ -13,7 +13,7 @@ import {
 import Image from 'react-bootstrap/Image'
 import { MDBRipple } from 'mdb-react-ui-kit';
 import { MDBBtn } from 'mdb-react-ui-kit';
-
+import { MDBFooter } from 'mdb-react-ui-kit';
 
 
 function home () {
@@ -26,12 +26,7 @@ function home () {
                 <link rel = "stylesheet" href = "/css/home.css" />
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
-                <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet"></link>
-                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
-                <link href="https://fonts.googleapis.com/css2?family=Festive&family=Fjalla+One&display=swap" rel="stylesheet"></link>
-
-                
+                <link href="https://fonts.googleapis.com/css2?family=Festive&family=Fjalla+One&family=Syne+Tactile&family=Yellowtail&display=swap" rel="stylesheet"></link>
             </head>
             <body>
             <header>
@@ -55,10 +50,10 @@ function home () {
                 </MDBNavbarLink>
               </MDBNavbarItem>
               <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+                <MDBNavbarLink href='/places'>Places</MDBNavbarLink>
               </MDBNavbarItem>
               <MDBNavbarItem>
-                <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+                <MDBNavbarLink href='/places/new'>Add Place</MDBNavbarLink>
               </MDBNavbarItem>
               <MDBNavbarItem>
                 <MDBNavbarLink href='#'>About</MDBNavbarLink>
@@ -75,7 +70,7 @@ function home () {
               <h1 className='mb-3 '>Please Ramen Calm...</h1>
               <h4 className='mb-3'>Push button below if your signficant other is HANGRY</h4>
               <a className='mb-3 btn btn-outline-light btn-lg' href="/places" role='button'>
-                Find Place</a>
+               ~~~~~~Find Place~~~~~~</a>
             </div>
             
           </div>
@@ -84,16 +79,132 @@ function home () {
 
         <div className='Donutpic'>
           <div className='donutstuff'>
-            <div className='text-white1'>
-              <h1 className='mb-3t '>If you donut mind...</h1>
-              <h4 className='mb-3t'>add a new restaurant!</h4>
-              <a className='RamenBtn' href="/places/new" role='button'>
-                Find Place
-              </a>
+          
+            <h1 className='donutp1'>If</h1>
+            <h1 className='donutp2'>you</h1>
+            <h1 className='donutp3'>donut</h1>
+            <h1 className='donutp4'>mind</h1>
+            <h1 className='donutp5'>sharing</h1>
+            <a className='donuteB' href="/places/new" role='button'>A New Restaurant</a>
+          </div>
+        </div>
+        <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+          <span>I'll work on this more later lol:</span>
+        </div>
+
+        <div>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-facebook-f'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-twitter'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-google'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-instagram'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-linkedin'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-github'></i>
+          </a>
+        </div>
+      </section>
+
+      <section className=''>
+        <div className='container text-center text-md-start mt-5'>
+          <div className='row mt-3'>
+            <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                <i className='fas fa-gem me-3'></i>CGVG
+              </h6>
+              <p>
+              I love my girlfriend
+              </p>
+            </div>
+
+            <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Ill add future projects here</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  hi
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  hello
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  its 3AM
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  im so sleepy
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Pricing
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Settings
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Orders
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Help
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <p>
+                <i className='fas fa-home me-3'></i> Follow my mental health account on Instagram
+              </p>
+              <p>
+                <i className='fas fa-envelope me-3'></i>
+                @cjcares
+              </p>
+              <p>
+                <i className='fas fa-phone me-3'></i> c4093galacgac@gmail.com
+              </p>
+              <p>
+                <i className='fas fa-print me-3'></i> Cj Krazy Kiwi on Twitch
+              </p>
             </div>
           </div>
         </div>
-            </body>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.0)' }}>
+        © 2021 Copyright:
+        <a className='text-reset fw-bold' style={{ backgroundColor: 'rgba(0, 0, 0, 0.0)' }} href='https://mdbootstrap.com/'>
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
+
+        </body>
         </html>
 
     )}
