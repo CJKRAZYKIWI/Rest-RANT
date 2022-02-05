862908d241804861c312c0dd02e38198f27ba7d1
Cj Galacgac <galacc1@unlv.nevada.edu>
2022-02-04 22:31:46 -0800

    finished ramen home page

--- views/home.jsx
+++ views/home.jsx
@@ -12,6 +12,7 @@ import {
 } from 'mdb-react-ui-kit';
 import Image from 'react-bootstrap/Image'
 import { MDBRipple } from 'mdb-react-ui-kit';
+import { MDBBtn } from 'mdb-react-ui-kit';
 
 
 
@@ -26,6 +27,9 @@ function home () {
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
                 <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet"></link>
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Festive&family=Fjalla+One&display=swap" rel="stylesheet"></link>
 
                 
             </head>
@@ -64,35 +68,31 @@ function home () {
         </MDBContainer>
       </MDBNavbar>
       </header>
-      <div className='ramenC'>
-        <div className='Ramenpic'style={{ backgroundImage: "url('https://images.unsplash.com/photo-1470324161839-ce2bb6fa6bc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80')"}}>
+        <div className='Ramenpic'>
           <div className='ramenstuff'>
-            <div className='text-white'>
+            <div className='text-white'> 
+            
               <h1 className='mb-3 '>Please Ramen Calm...</h1>
               <h4 className='mb-3'>Push button below if your signficant other is HANGRY</h4>
               <a className='mb-3 btn btn-outline-light btn-lg' href="/places" role='button'>
-                Find Place
-              </a>
+                Find Place</a>
             </div>
+            
           </div>
         </div>
-      </div>
    
-    <div
-        className='p-8 text-center bg-image'
-      >
-        <div className='mask1' style={{ backgroundImage: "url('https://images.unsplash.com/flagged/photo-1621757458931-a1b076e5a8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80')", height:700,}}>
+
+        <div className='Donutpic'>
           <div className='donutstuff'>
             <div className='text-white1'>
               <h1 className='mb-3t '>If you donut mind...</h1>
               <h4 className='mb-3t'>add a new restaurant!</h4>
-              <a className='btn btn-outline-dark btn-lg' href="/places/new" role='button'>
+              <a className='RamenBtn' href="/places/new" role='button'>
                 Find Place
               </a>
             </div>
           </div>
         </div>
-      </div>
             </body>
         </html>
 
